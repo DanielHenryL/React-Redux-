fff7925c0a673678e308dbdae982cc3a02bbcb59
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-04-17 23:25:50 -0500

    configuracion de la bd

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,27 @@
-console.log('Hola mundo')
+const express = require('express');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+
+
+// Crear servidor express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// Directorio publico
+app.use( express.static('public'))
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas 
+app.use( '/api/auth', require('./routes/auth'))
+// TODO: CRUD: Events
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
